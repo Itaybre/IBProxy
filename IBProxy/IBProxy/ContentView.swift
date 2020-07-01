1f85a2dd8639b3e77fb50b10184a088c450a522e
Itay <itay@itaysoft.com>
2020-07-01 00:06:49 -0300

    Add Xcode project

---
+++ IBProxy/IBProxy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IBProxy
+//
+//  Created by Itay Brenner on 7/1/20.
+//  Copyright © 2020 Itaysoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
