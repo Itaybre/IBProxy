22641340f85211950a1fd317897abd1df4f3302d
Itay <itay@itaysoft.com>
2020-08-12 01:03:49 -0300

    Use UIKit instead of SwiftUI

---
+++ IBProxy/Views/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  IBProxy
+//
+//  Created by Itay Brenner on 8/12/20.
+//  Copyright © 2020 Itaysoft. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "IBProxy"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
